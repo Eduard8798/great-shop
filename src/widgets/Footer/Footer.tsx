43cc4af6fab6090fa834203e9ad0464838217781
Kolya Kulchytskyi <lircusgaming@gmail.com>
2026-02-24 20:12:40 +0200

    add signup page UI without logic

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-white p-4 mt-10">
+        <footer className="bg-gray-800 text-white p-4">
             <div className="container mx-auto text-center">
                 <p>© 2026 Great Shop. All rights reserved.</p>
                 <p>
