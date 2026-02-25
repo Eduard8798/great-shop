11e8434e084a83d7c9c9a018dde0df79f4d8f413
Eduard8798 <eduard9999997@gmail.com>
2026-02-25 17:37:40 +0200

    page login

--- src/components/ui/LoginForm/LoginForm.tsx
+++ src/components/ui/LoginForm/LoginForm.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "../LoginForm/Login.module.scss";
 import girlLogin from "../../../../public/girlLogin.png";
+import {FcGoogle} from "react-icons/fc";
+import {SiGmail} from 'react-icons/si';
+
 
 export default function LoginForm() {
     return (
@@ -17,42 +20,48 @@ export default function LoginForm() {
 
             {/* First side with form */}
             <div className={styles.right}>
-                <h1 className={styles.title}>Great Shop</h1>
-                <h2 className={styles.subtitle}>Sign in Great Shop</h2>
+                <div className={styles.rigthWidth}>
+                    <h1 className={styles.title}>Great Shop</h1>
+                    <h2 className={styles.subtitle}>Sign in Great Shop</h2>
 
-                <div className={styles.socialButtons}>
-                    <button className={styles.googleBtn}>
-                        Sign in with Google
-                    </button>
-                    <button className={styles.emailBtn}>
-                        Sign in with Email
-                    </button>
-                </div>
+                    <div className={styles.socialButtons}>
+                        <button className={styles.googleBtn}>
+                            <FcGoogle size={28}/>
+                            Sign up in with Google
+                        </button>
+                        <button className={styles.emailBtn}>
+                            <SiGmail size={28} color="#EA4335"/>
+                            Sign up with Email
+                        </button>
+                    </div>
 
-                <div className={styles.or}>
-                    <p className={styles.line}>-</p>
-                    <p className={styles.text}>OR</p>
-                    <p className={styles.line}>-</p>
-                </div>
+                    <div className={styles.or}>
+                        <p className={styles.line}>-</p>
+                        <p className={styles.text}>OR</p>
+                        <p className={styles.line}>-</p>
+                    </div>
 
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className={styles.inputField}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className={styles.inputField}
-                />
-                <div className={styles.topBnt}>
-                    <button className={styles.signInBtn}>Sign In</button>
-                    <button className={styles.registerBtn}>Register Now</button>
-                </div>
-                <div className={styles.forgot}>
-                    <a href="#">Forgot Password?</a>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className={styles.inputField}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className={styles.inputField}
+                    />
+                    <div className={styles.forgot}>
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <div className={styles.topBnt}>
+                        <button className={styles.signInBtn}> Sign In</button>
+                        <button className={styles.registerBtn}>Register Now</button>
+                    </div>
+
                 </div>
             </div>
         </div>
     );
 }
+
