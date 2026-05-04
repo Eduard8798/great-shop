1159a7bd0ec95752c1e8beaad1214427e927cd9e
MacMini <MacMini@Mac-mini.local>
2026-05-04 17:57:21 +0300

    add OrderFormList , add switch menu-top

--- src/features/auth/ui/ProfileForm/ProfileForm.tsx
+++ src/features/auth/ui/ProfileForm/ProfileForm.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 import styles from './Profile.module.scss'
 import {bell, compited, exit, location, orders, user} from "@/features/auth/ui/ProfileForm/icon/ProfileIcon";
 import ProfileFormList from "@/features/auth/ui/ProfileForm/ProfileFormList/ProfileFormList";
+import OrderFormList from "@/features/auth/ui/ProfileForm/OrderFormList/OrderFormList";
 
 const ProfileForm = () => {
     type MenuItem = {
@@ -11,46 +12,59 @@ const ProfileForm = () => {
         icon: React.ReactNode;
     }
 
+
+    const tabMenuContent: Record<string, ReactNode> = {
+        profile: <ProfileFormList/>,
+        orders: <OrderFormList/>
+    }
+    const [activeTab, setActiveTab] = useState<string>("profile")
+
     const [listMain, setListMain] = useState<MenuItem[]>([
-        {id: 'profile', name: 'Profile', active: false,icon: user},
-        {id: 'bonuses', name: 'Bonuses', active: false,icon: compited},
-        {id: 'orders', name: 'My orders', active: false,icon: orders},
-        {id: 'addresses', name: 'Addresses', active: false,icon: location},
-        {id: 'notifications', name: 'Notifications', active: false,icon: bell},
-        {id: 'out', name: 'Log out', active: false,icon: exit},
+        {id: 'profile', name: 'Profile', active: false, icon: user},
+        {id: 'bonuses', name: 'Bonuses', active: false, icon: compited},
+        {id: 'orders', name: 'My orders', active: false, icon: orders},
+        {id: 'addresses', name: 'Addresses', active: false, icon: location},
+        {id: 'notifications', name: 'Notifications', active: false, icon: bell},
+        {id: 'out', name: 'Log out', active: false, icon: exit},
     ])
 
-    function changeActive (id:string){
+    function changeActive(id: string) {
 
-        setListMain(item => item.map(e =>
-            {
-                if (e.id === id){
+        setListMain(item => item.map(e => {
+                if (e.id === id) {
                     return {...e, active: true}
-                }
-                else {
-                   return {...e,active:false}
+                } else {
+                    return {...e, active: false}
                 }
             }
         ))
     }
 
+    function changeMenu() {
+
+    }
+
     return (
         <>
-        <div className={styles.headerProfile}>
-            <div className={styles.main}>
+            <div className={styles.headerProfile}>
+                <div className={styles.main}>
 
-                <div className={styles.nameUser}>
-                   <div>Hello</div>
-                   <span className={styles.user}>John Smith</span>
+                    <div className={styles.nameUser}>
+                        <div>Hello</div>
+                        <span className={styles.user}>John Smith</span>
+                    </div>
+                    {listMain.map(value => (
+                        <div key={value.id} className={value.active ? styles.listItemMainActive : styles.listItemMain}
+                             onClick={() => {
+                                 changeActive(value.id);
+                                 setActiveTab(value.id);
+                             }}
+                        >{value.icon}{value.name}</div>
+                    ))}
                 </div>
-                {listMain.map(value => (
-                    <div key={value.id} className={value.active ? styles.listItemMainActive : styles.listItemMain}
-                     onClick={()=>changeActive(value.id)}
-                    >{value.icon}{value.name}</div>
-                ))}
+                {/*<ProfileFormList/>*/}
+                {tabMenuContent[activeTab]}
             </div>
-            <ProfileFormList/>
-        </div>
 
         </>
     );
@@ -59,6 +73,4 @@ const ProfileForm = () => {
 
 export default ProfileForm;
 
-//  обробник на клік на меню - акт - збергти Ід
-//
-// первірити ід із збереженим
+
